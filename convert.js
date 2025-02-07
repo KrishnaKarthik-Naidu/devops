50380c94728034bdf415b46b223035e47ed85235
Krishna Karthik Naidu <krishnakarthiknaidu@Krishnas-MacBook-Air.local>
2025-02-07 15:17:19 +0530

    api

--- convert.js
+++ convert.js
@@ -1,1 +1,9 @@
-console.log("heloo");
+const EXCHANGE_RATE = 83.0;
+const convert = inr => {
+if (isNaN(inr)) {
+throw "Invalid amount";
+} else {
+return (inr / EXCHANGE_RATE).toFixed(2);
+}
+}
+module.exports = convert;
